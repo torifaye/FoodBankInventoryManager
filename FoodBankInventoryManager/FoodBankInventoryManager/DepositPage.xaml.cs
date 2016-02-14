9b458c19e005cdf4d07bcae853addad8f3b684dd
Robert Yost <bobby.9202@gmail.com>
2016-02-14 00:32:20 -0500

    Add sorting options to combo box

--- FoodBankInventoryManager/FoodBankInventoryManager/DepositPage.xaml.cs
+++ FoodBankInventoryManager/FoodBankInventoryManager/DepositPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq.Mapping;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,24 @@ namespace FoodBankInventoryManager
     /// </summary>
     public partial class DepositPage : Page
     {
+        private L2S_FoodBankDBDataContext dbContext;
         public DepositPage()
         {
             InitializeComponent();
+            dbContext = new L2S_FoodBankDBDataContext();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //useful if you need to get data from the database such as table names
+            //var dataModel = new AttributeMappingSource().GetModel(typeof(L2S_FoodBankDBDataContext));
+            string[] sortOptions = new string[] { "Item Name (A-Z)", "Item Name (Z-A)", "Quantity (Asc.)","Quantity (Desc.)" };
+            foreach (string item in sortOptions)
+            {
+                comboSort.Items.Add(item);
+            }
+            comboSort.SelectedIndex = 0;
+               
         }
     }
 }

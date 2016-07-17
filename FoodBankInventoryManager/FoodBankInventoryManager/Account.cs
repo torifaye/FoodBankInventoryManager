845a6c4f41778bb9b75741769d09f52f1d5e404b
Robert Yost <bobby.9202@gmail.com>
2016-07-17 14:59:52 -0400

    Create account class

---
+++ FoodBankInventoryManager/FoodBankInventoryManager/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodBankInventoryManager
+{
+    class Account
+    {
+        private static uint myUserId;
+        private string myFirstName;
+        private string myLastName;
+        private string myPassword;
+        private string myEmail;
+        private bool myAccessLevel;
+
+        public Account()
+        {
+            myUserId++;
+        }
+        public static uint UserID
+        {
+            get { return myUserId; }
+        }
+        public string FirstName
+        {
+            get { return myFirstName; }
+            set { myFirstName = value; }
+        }
+        public string LastName
+        {
+            get { return myLastName; }
+            set { myLastName = value; }
+        }
+        public string Email
+        {
+            get { return myEmail; }
+            set { myEmail = value; }
+        }
+        public bool IsAdmin
+        {
+            get { return myAccessLevel; }
+            set { myAccessLevel = value; }
+        }
+
+    }
+}

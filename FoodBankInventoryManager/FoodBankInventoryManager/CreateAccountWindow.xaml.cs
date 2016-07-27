6bd90e7388e2b77d1c9a6a54c7b30fadad7625e7
Robert <bobby.9202@gmail.com>
2016-07-26 23:41:30 -0400

    Check email format validity

--- FoodBankInventoryManager/FoodBankInventoryManager/CreateAccountWindow.xaml.cs
+++ FoodBankInventoryManager/FoodBankInventoryManager/CreateAccountWindow.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Net;
 using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace FoodBankInventoryManager
 {
@@ -25,7 +18,7 @@ namespace FoodBankInventoryManager
         private L2S_FoodBankDBDataContext dbContext;
         private User myAccount;
         private int comboBoxChoice;
-
+        private bool emailIsInvalid;
 
         public CreateAccountWindow()
         {
@@ -33,6 +26,7 @@ namespace FoodBankInventoryManager
             emptyFields = new bool[7];
             dbContext = new L2S_FoodBankDBDataContext();
             myAccount = new User();
+            emailIsInvalid = false;
         }
 
         private void cBoxAccessLevel_Loaded(object sender, RoutedEventArgs e)
@@ -55,11 +49,22 @@ namespace FoodBankInventoryManager
             //and then alert them
             myAccount.FirstName = txtFirstName.Text;
             myAccount.LastName = txtLastName.Text;
-            //TODO: Confirm that the email that they give is valid
-            if (txtEmail.Text == txtConfirmEmail.Text)
+            /*
+             * Makes sure that the text in both fields match and that the text in both
+             * fields is in a valid format before setting the account's email as the
+             * user's input
+             */
+            if (txtEmail.Text == txtConfirmEmail.Text 
+                && isValidEmailFormat(txtEmail.Text) 
+                && isValidEmailFormat(txtConfirmEmail.Text))
             {
                 myAccount.Email = txtEmail.Text;
             }
+            /*
+             * Encrypts and salts a user's password using BCrypt. Might eventually want
+             * to change to PBKDF2 since that's the only NIST-approved encryption algorithm
+             * for C#
+             */
             string mySalt = BCrypt.GenerateSalt();
             string myHash = BCrypt.HashPassword(pwBoxPassword.Password, mySalt);
             if (BCrypt.CheckPassword(pwBoxConfirmPassword.Password, myHash))
@@ -112,6 +117,57 @@ namespace FoodBankInventoryManager
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private bool isValidEmailFormat(string anEmail)
+        {
+            if (String.IsNullOrEmpty(anEmail))
+            {
+                return false;
+            }
+            try
+            {
+                anEmail = Regex.Replace(anEmail, @"(@)(.+)$", this.DomainMapper,
+                                RegexOptions.None, TimeSpan.FromMilliseconds(200));
+            }
+            catch (RegexMatchTimeoutException ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+
+            if (emailIsInvalid)
+            {
+                return false;
+            }
+            try
+            {
+                return Regex.IsMatch(anEmail,
+                      @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                      RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        private string DomainMapper(Match match)
+        {
+            // IdnMapping class with default property values.
+            IdnMapping idn = new IdnMapping();
+
+            string domainName = match.Groups[2].Value;
+            try
+            {
+                domainName = idn.GetAscii(domainName);
+            }
+            catch (ArgumentException)
+            {
+                emailIsInvalid = true;
+            }
+            return match.Groups[1].Value + domainName;
         }
     }
 }

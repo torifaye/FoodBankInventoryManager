58a0f234587dd04ca64dd249f1c43469677cc05c
Michael Kaltman <michaelman248@gmail.com>
2016-08-07 16:24:41 -0400

    audit changes

--- FoodBankInventoryManager/FoodBankInventoryManager/AuditPage.xaml.cs
+++ FoodBankInventoryManager/FoodBankInventoryManager/AuditPage.xaml.cs
@@ -56,6 +56,7 @@ namespace FoodBankInventoryManager
             HomePage h = new HomePage(myCurrentUser);
             NavigationService.Navigate(h);
         }
+
     }
     public class AuditInfo
     {
